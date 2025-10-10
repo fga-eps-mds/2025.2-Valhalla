d022cf96e117efb216117fe4de899074770413a5
dev-americo <dev.americo@gmail.com>
2025-10-10 17:48:48 -0300

    refactor - padronizacao em cabelcode

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UsuarioService } from './usuario/usuario.service';
+//import { UsuarioService } from './usuario/usuario.service';
 import { DenunciaService } from './denuncia/denuncia.service';
 import { DenunciaController } from './denuncia/denuncia.controller';
 import { DenunciaModule } from './denuncia/denuncia.module';
@@ -8,6 +8,6 @@ import { DenunciaModule } from './denuncia/denuncia.module';
 @Module({
   imports: [DenunciaModule],
   controllers: [DenunciaController],
-  providers: [UsuarioService, DenunciaService],
+  providers: [DenunciaService],
 })
 export class AppModule {}
