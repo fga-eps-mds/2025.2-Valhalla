e98e06ff5921b0b3a9c591419fa86de36421b5f6
Lucas Alves <lucastouram@gmail.com>
2025-10-10 22:50:45 -0300

    Sync(app.module.ts):Merge do app.module.ts para resolver conflito

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
+import { DenunciaService } from './denuncia/denuncia.service';
+import { DenunciaController } from './denuncia/denuncia.controller';
+import { DenunciaModule } from './denuncia/denuncia.module';
 import { UsuarioService } from './usuario/usuario.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
-
-
 @Module({
-  imports: [UsuarioModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DenunciaModule,UsuarioModule],
+  controllers: [DenunciaController,AppController],
+  providers: [DenunciaService,AppService],
 })
 export class AppModule {}
