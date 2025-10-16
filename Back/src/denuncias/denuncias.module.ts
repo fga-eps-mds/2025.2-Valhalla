975abc5f125b59b266c076cd8cae3bbd2cc3b712
dev-americo <dev.americo@gmail.com>
2025-10-16 18:28:51 -0300

    feat - criacao service denuncias
    Co-authored-by: juliagabriellafs <juliagabriellafs@users.noreply.github.com>

--- Back/src/denuncias/denuncias.module.ts
+++ Back/src/denuncias/denuncias.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DenunciasController } from './denuncias.controller';
+import { DenunciasService } from './denuncias.service'
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
+  providers: [DenunciasService, PrismaService],
   controllers: [DenunciasController]
 })
 export class DenunciasModule {}
