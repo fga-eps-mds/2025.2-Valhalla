4f14a7f174ed588be1380e17ab6ca9e2bf3c51cf
pedroiaan <pedroianx8@gmail.com>
2025-10-23 23:21:57 -0300

    feat - adição do decorador ispublic nas determinadas funções

--- Back/src/auth/auth.module.ts
+++ Back/src/auth/auth.module.ts
@@ -4,11 +4,13 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsuarioModule } from 'src/usuario/usuario.module';
 import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
-  imports: [UsuarioModule, JwtModule.register({
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  imports: [UsuarioModule, ConfigModule, JwtModule.register({
      secret: process.env.JWT_SECRET,
      signOptions: { expiresIn: '30d'},
   })]
--- Back/src/usuario/usuario.controller.ts
+++ Back/src/usuario/usuario.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Body, Post, Delete, Param, UseGuards, Get, Patch, ParseIntP
 import { UsuarioDto } from './dto/usuario.dto';
 import { UsuarioService } from './usuario.service';
 import { updateUsuarioDto } from './dto/update.usuario.dto';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('Usuario')
 export class UsuarioController {
     
     constructor (private readonly usuarioService: UsuarioService){}
 
+    @IsPublic()
     @Post()
     async create(@Body() data:UsuarioDto){
         return this.usuarioService.CriarUsuario(data);
@@ -17,12 +19,12 @@ export class UsuarioController {
     async delete(@Param("id") id:number) {
         return this.usuarioService.DeletarUsuario(Number(id));
     }
-
+    @IsPublic()
     @Get(":id")
     async FindOne(@Param("id") id:number) {
         return this.usuarioService.FindOne(Number(id));
     }
-    
+    @IsPublic()
     @Get()
     async findAll() {
         return this.usuarioService.findAll();
