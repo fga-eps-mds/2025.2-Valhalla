d27b087eb9a1ad2f3a0ea28508b2c0399e8a29df
Lucas Alves <lucastouram@gmail.com>
2025-10-23 23:35:54 -0300

    build:instala biblioteca de icons

--- Front/src/app/cadastro/page.tsx
+++ Front/src/app/cadastro/page.tsx
@@ -1,7 +1,7 @@
 export default function Cadastro() {
     return (
         <>
-        <div className="w-[40vw] h-[80vh] bg-white rounded-[1rem] opacity-80 shadow-[0_0.25rem_0.25rem_0_rgba(0,0,0,0.25)] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="w-[40vw] h-[80vh] bg-white rounded-[1rem] opacity-80 shadow-[0_0.25rem_0.25rem_0_rgba(0,0,0,0.25)] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center">
             <h1>Cadastro</h1>
         </div>
         </>
