f37aee01348233b817953934da805c1c740f42ed
Lucas Alves <lucastouram@gmail.com>
2025-10-24 02:04:16 -0300

    feat: cria os campos de input para o usuario se cadastrar

--- Front/src/app/cadastro/page.tsx
+++ Front/src/app/cadastro/page.tsx
@@ -1,10 +1,10 @@
-import { BeakerIcon } from '@heroicons/react/24/solid'
+import { AcademicCapIcon, UserCircleIcon, KeyIcon, ChevronUpDownIcon,ShieldCheckIcon,ArrowRightEndOnRectangleIcon, ArrowLeftIcon } from '@heroicons/react/24/outline'
 import Image from "next/image";
 
 export default function Cadastro() {
     return (
         <>
-        <div className="w-[40vw] h-[80vh] bg-white rounded-[1rem] opacity-80 shadow-[0_0.25rem_0.25rem_0_rgba(0,0,0,0.25)] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center">
+        <div className="w-[40vw] h-[80vh] bg-white rounded-[1rem] opacity-80 shadow-[0_0.25rem_0.25rem_0_rgba(0,0,0,0.25)] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center pb-6">
             <Image 
             src="/Corujuda.svg"
             alt='Logo do Guardiões da Universidade. Uma coruja com pelagem azul'
@@ -13,6 +13,52 @@ export default function Cadastro() {
             className="mx-[]"
             />
             <h1>Cadastro</h1>
+
+            <ul className='flex flex-col gap-2 px-6' >
+                <li className='flex items-center border'>
+                    <AcademicCapIcon className='size-6'/>
+                    <input type="text" 
+                    placeholder='Digite aqui seu Nome'
+                    className="w-full outline-none"
+                    />
+                </li>
+                
+                <li className='flex items-center border'>
+                    <UserCircleIcon className='size-6'/>
+                    <input type="text" 
+                    placeholder='Digite aqui seu Email'
+                    className="w-full outline-none"
+                    />
+                </li>
+                
+                <li className='flex items-center border'>
+                    <KeyIcon className='size-6'/>
+                    <input type="password" 
+                    placeholder='Digite aqui sua Senha'
+                    className="w-full outline-none"
+                    />
+                </li>
+                
+                <li className='flex items-center border'>
+                    <KeyIcon className='size-6'/>
+                    <input type="password" 
+                    placeholder='Digite aqui sua Senha'
+                    className="w-full outline-none"
+                    />
+                </li>
+                
+                <li className='flex items-center border'>
+                    <ChevronUpDownIcon className='size-6'/>
+                    <select className='w-full outline-none'>
+                        <option value="" disabled selected>Selecione sua Ocupação</option>
+                        <option value="Servidor">Servidor</option>
+                        <option value="Aluno">Aluno</option>
+                    </select>
+                    
+                    
+                </li>
+            </ul>
+
         </div>
         </>
     )
