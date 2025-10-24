b40c8a45ad62e1206ad35635425f7259f367a4ad
Lucas Alves <lucastouram@gmail.com>
2025-10-23 23:11:39 -0300

    feat: cria e estiliza a div na pagina de Cadastro

---
+++ Front/src/app/cadastro/page.tsx
@@ -0,0 +1,9 @@
+export default function Cadastro() {
+    return (
+        <>
+        <div className="w-[40vw] h-[80vh] bg-white rounded-[1rem] opacity-80 shadow-[0_0.25rem_0.25rem_0_rgba(0,0,0,0.25)] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <h1>Cadastro</h1>
+        </div>
+        </>
+    )
+}
