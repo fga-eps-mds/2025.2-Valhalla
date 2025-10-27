4807d947c804a8e8ed401f453770f594b7a4eb78
Pedro Lucas <21pedrolucas21@gmail.com>
2025-10-27 17:02:13 -0300

    test - Configurando teste para Categorias

---
+++ Back/jest.setup.ts
@@ -0,0 +1,5 @@
+// jest.setup.ts
+import '@testing-library/jest-dom/extend-expect';
+
+// se usar next/router em componentes cliente, você pode mockar aqui:
+// jest.mock('next/router', () => require('next-router-mock'));
