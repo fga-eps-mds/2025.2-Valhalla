d9e7db2cbf84acfefdffb5605dc3f72f69ba43bb
pedroiaan <pedroianx8@gmail.com>
2025-10-29 18:50:43 -0300

    feat - criação do método no controller
    Co-authored-by: Devv-Antonio  antonio.lucasa06@gmail.com

--- Back/src/auth/auth.controller.ts
+++ Back/src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Patch, Post, Req, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/authRequest';
 import { IsPublic } from './decorators/is-public.decorator';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { MudarSenhaDto } from './models/mudar-senhaDto';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +16,11 @@ export class AuthController {
     @UseGuards(LocalAuthGuard)
     login(@Request() req: AuthRequest){
         return this.authService.login(req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch('mudar-senha')
+    async mudarSenha(@Req() req, @Body() MudarSenhaDto: MudarSenhaDto){
+        return this.authService.mudarSenha(req.user.id, MudarSenhaDto.senhaAntiga, MudarSenhaDto.senhaNova);
     }
 }
