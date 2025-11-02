8c6705e184685d2fc5e1f75a9a41d9cc9198819e
pedroiaan <pedroianx8@gmail.com>
2025-11-02 14:14:37 -0300

    feat - criação das funções de reset senha e esqueci senha no auth service
    Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/auth/auth.service.ts
+++ Back/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { UsuarioService } from 'src/usuario/usuario.service';
 import * as bcrypt from 'bcrypt';
 import { UsuarioDto } from 'src/usuario/dto/usuario.dto';
@@ -6,9 +6,12 @@ import { JwtService } from "@nestjs/jwt";
 import { UsuarioPayload } from './models/UsuarioPayload';
 import { UsuarioToken } from './models/UsuarioToken';
 import { updateUsuarioDto } from 'src/usuario/dto/update.usuario.dto';
+import { MailService } from 'src/mail/mail.service';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthService {
-    constructor(private readonly usuarioService: UsuarioService, private readonly jwtService: JwtService){}
+    constructor(private readonly usuarioService: UsuarioService, private readonly jwtService: JwtService, private readonly mailService: MailService,
+      private readonly configService: ConfigService){}
 
      login(usuario: UsuarioDto): UsuarioToken {
         const payload: UsuarioPayload = {
@@ -58,6 +61,59 @@ export class AuthService {
         await this.usuarioService.update(usuario.id, dadosParaAtualizar);
         
     }
+    async esqueciSenha(email: string): Promise<{ message: string }> {
+      const usuario = await this.usuarioService.procurarPorEmail(email);
 
-    
+      if (!usuario) {
+        return { message: 'Se um utilizador com esse email existir, um link de redefinição será enviado.' };
+      }
+
+      // 1. Criar o payload para o token de reset
+      const payload: UsuarioPayload = { 
+        sub: usuario.id!,
+        email: usuario.email,
+        nome: usuario.nome
+      };
+      
+      const resetSecret = this.configService.get<string>('JWT_PASSWORD_RESET_SECRET');
+      const expiresIn = '15m';
+
+      const token = this.jwtService.sign(payload, {
+        secret: resetSecret,
+        expiresIn: expiresIn,
+      });
+
+      try {
+        await this.mailService.sendPasswordResetEmail(usuario, token);
+      } catch (error) {
+        console.error('Falha ao enviar email:', error);
+        throw new BadRequestException('Não foi possível enviar o email de redefinição.');
+      }
+
+      return { message: 'Se um utilizador com esse email existir, um link de redefinição será enviado.' };
+    }
+    async resetSenha(token: string, novaSenha: string): Promise<{ message: string }> {
+      const resetSecret = this.configService.get<string>('JWT_PASSWORD_RESET_SECRET');
+
+      try {
+        const payload = this.jwtService.verify(token, {
+          secret: resetSecret,
+        }) as UsuarioPayload; 
+
+
+        const userId = payload.sub;
+
+        const novaSenhahash = await bcrypt.hash(novaSenha, 10);
+
+        const dadosParaAtualizar = new updateUsuarioDto();
+        dadosParaAtualizar.senha = novaSenhahash;
+
+        await this.usuarioService.update(userId, dadosParaAtualizar);
+
+        return { message: 'Senha redefinida com sucesso!' };
+
+      } catch (error) {
+        throw new ForbiddenException('Token inválido ou expirado.');
+      }
+    }
 }
