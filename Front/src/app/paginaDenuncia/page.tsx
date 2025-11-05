9343716a81e864f4182b485f802ce6e66890ea3f
juliagabriellafs <juliagabriellaf0407@gmail.com>
2025-11-05 05:53:23 -0300

    feat - inicializa estrutura da página com imports básicos do React e Next
    Co-authored-by: Gustavo2703 <Gustavo2703@users.noreply.github.com>

---
+++ Front/src/app/paginaDenuncia/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+export default function PaginaDenuncias() {
+
+  return null;
+}
