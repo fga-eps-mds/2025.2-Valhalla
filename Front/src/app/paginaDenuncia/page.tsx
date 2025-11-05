84ad58e49fddbee5b85fa31892bbb9f6fe1a2a39
juliagabriellafs <juliagabriellaf0407@gmail.com>
2025-11-05 06:02:26 -0300

    feat - importa icones heroicons para pagina de denuncias
    Co-authored-by: Gustavo2703 <Gustavo2703@users.noreply.github.com>

--- Front/src/app/paginaDenuncia/page.tsx
+++ Front/src/app/paginaDenuncia/page.tsx
@@ -1,8 +1,19 @@
 'use client';
 
+// Imports ESTRUTURAIS do React e Next
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+
+// Imports VISUAIS
+import {
+  BookOpenIcon,
+  NewspaperIcon,
+  HomeIcon,
+  ArchiveBoxIcon,
+  UserCircleIcon,
+  PlusIcon,
+} from '@heroicons/react/24/solid';
 
 export default function PaginaDenuncias() {
 
