36750320caae907727badcd719994f95a0a863b0
juliagabriellafs <juliagabriellaf0407@gmail.com>
2025-11-05 07:59:47 -0300

    feat - estiliza botão de ação flutuante (FAB)
    Co-authored-by: Gustavo2703 <Gustavo2703@users.noreply.github.com>

--- Front/src/app/paginaDenuncia/page.tsx
+++ Front/src/app/paginaDenuncia/page.tsx
@@ -15,14 +15,17 @@ import {
   PlusIcon,
 } from '@heroicons/react/24/solid';
 
+
 export default function PaginaDenuncias() {
 
   return (
+    // Container principal da página
     <div className="bg-white min-h-screen relative">
+
         {/*Cabeçalho*/}
       <div className="bg-[#1A293F] h-21 shadow-md flex items-center justify-between pl-5 pr-15">
+        
         {/*Conteúdo do header*/}
-
         {/*Esquerda: Logo e Título */}
         <div className="flex items-center space-x-4">
           <Image
@@ -60,8 +63,26 @@ export default function PaginaDenuncias() {
       </main>
 
       {/* Botão flutuante */}
-      <button aria-label="Nova Denúncia">
-        <PlusIcon />
+      <button 
+      aria-label="Nova Denúncia"
+      className="
+          fixed                   /* Fixo na tela */
+          bottom-14               /* Posição: 56px de baixo */
+          right-[50px]            /* Posição: 50px da direita */
+          h-26                    /* Altura: 104px */
+          w-26                    /* Largura: 104px */
+          rounded-full            /* Totalmente redondo */
+          bg-[#1A2A4A]          /* Cor de fundo */
+          hover:bg-[#67A8FF]    /* Cor ao passar o mouse */
+          flex                    /* Para centralizar o ícone */
+          items-center            /* ...verticalmente */
+          justify-center          /* ...horizontalmente */
+          shadow-lg               /* Sombra */
+          transition-colors       /* Efeito de transição */
+          duration-300
+        "
+      >
+        <PlusIcon className="h-12 w-12 text-white"/>
       </button>
 
     </div>
