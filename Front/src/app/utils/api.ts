32a43773afe133d5e8b14aa4c8bf272a30430627
pedroiaan <pedroianx8@gmail.com>
2025-11-10 10:41:41 -0300

    feat -  funcionalidade de login / tirando o node modules

---
+++ Front/src/app/utils/api.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const loginUsuario = async (email: string, senha: string) => {
+  const response = await api.post("/auth/login", { email, senha }); 
+  return response.data;
+};
+
+export const getOneUsuario = async (id: number) => {
+  const response = await api.get(`/Usuario/${id}`); 
+  return response.data;
+};
