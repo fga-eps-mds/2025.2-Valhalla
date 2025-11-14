361b4dc6d7b9adc577f185fc1d9efa310e24a35f
juliagabriellafs <juliagabriellaf0407@gmail.com>
2025-11-14 20:52:21 -0300

    feat - estiliza borda da inclusão de imagens e categoria de denuncias
    Co-authored-by: xLucasMelo <xLucasMelo@users.noreply.github.com>

--- Front/src/app/components/modalDenuncia/modalDenuncia.tsx
+++ Front/src/app/components/modalDenuncia/modalDenuncia.tsx
@@ -22,7 +22,7 @@ export default function ModalDenuncia ({isOpen}:DenunciaModalProps) {
                 <button className='w-[135px] h-[45px] border rounded-[46px] text-small'>PÚBLICA</button>
               
               </div>
-              <div className='w-[366px] h-[52px] border-[var(--color-bordas)] rounded-[10px] flex items-center '>
+              <div className='w-[366px] h-[52px] border border-[var(--color-bordas)] rounded-[10px] flex items-center '>
               <ChevronUpDownIcon className='size-[24px]'/>
               <select className=''>
                         <option value="" disabled selected>Selecione a Categoria</option>
@@ -30,7 +30,7 @@ export default function ModalDenuncia ({isOpen}:DenunciaModalProps) {
                         <option value="">Aluno</option>
                     </select>
               </div>
-              <div className='w-[256px] h-[159px] flex items-center justify-center border'>
+              <div className='w-[256px] h-[159px] flex items-center justify-center border border-[3px] border-dashed border-[var(--color-azul-principal)] rounded-[20px]'>
                 <CameraIcon className='size-[74px]'/>
                 <PlusIcon className='size-[41px]'/>
               </div>
@@ -42,5 +42,4 @@ export default function ModalDenuncia ({isOpen}:DenunciaModalProps) {
             </div>
             </>
         )
-    
 }
