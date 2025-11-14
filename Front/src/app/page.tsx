cfe43b9b8c3875ce48188040d6fdf4c0a2d9ac6a
Pedro Lucas <21pedrolucas21@gmail.com>
2025-11-14 14:27:13 -0300

    feature - adiciona hero da homepage

--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -6,23 +6,15 @@ import Image from "next/image";
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
-// Imports VISUAIS
-import {
-  BookOpenIcon,
-  NewspaperIcon,
-  HomeIcon,
-  ArchiveBoxIcon,
-  UserCircleIcon,
-  PlusIcon,
-} from '@heroicons/react/24/solid';
-
 export default function Home() {
   return (
-    <div>
-      <header className="bg-[#1A293F] h-17 shadow-md flex items-center justify-between pl-5 pr-15">
+    <main>
 
-        {/*Conteúdo do header*/}
-        {/*Esquerda: Logo e Título */}
+      {/*Header Inicio*/}
+
+      <header className="bg-texto-corpo h-17 shadow-md flex items-center justify-between pl-5 pr-15">
+
+        {/*Conteúdo do header*/} {/*Esquerda: Logo e Título */}
         <div className="flex items-center space-x-3">
           <Image
             src="/Corujuda-Contorno.svg"
@@ -34,8 +26,44 @@ export default function Home() {
             Guardiões da Universidade</h1>
         </div>
       </header>
-      <div> porra
+
+      {/*Header Fim*/}
+
+      <div className="flex justify-center">
+        <div className="w-full scroll-m-0 max-w-[1440px] bg-gray-100  rounded-md px-12 py-14">
+          {/* Conteúdo: texto à esquerda, imagem à direita */}
+          <div className="flex items-start my-12 gap-24">
+            {/* Coluna de texto */}
+            <div className="flex-1 max-w-xl">
+              <h1 className="text-h1">Guardiões da Universidade</h1>
+
+              <p className="text-medium text-justify">
+                O projeto Guardiões da Universidade foi desenvolvido para ser um canal de denúncia para a comunidade da Universidade de Brasília (UnB), sendo um meio para os discentes e os servidores exporem suas denúncias, conhecerem os procedimentos oficiais de denúncia e visualizarem as demandas da universidade.
+              
+              </p>
+              <div className="flex gap-4">
+                <button className="px-6 py-3 bg-gray-300 rounded-lg hover:bg-gray-400">
+                  Sobre Nós
+                </button>
+                <button className="px-6 py-3 bg-gray-300 rounded-lg hover:bg-gray-400">
+                  Denuncie
+                </button>
+              </div>
+            </div>
+            {/* Imagem */}
+            <div className=" shrink-0">
+              <Image
+                src="/FCTE.jpg"
+                alt="FCTE - Campus UnB Gama"
+                width={633}
+                height={473}
+                className="object-cover rounded-xl"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+
+    </main>
+  )
 }
