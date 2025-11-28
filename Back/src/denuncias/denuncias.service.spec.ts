02a95df094c9933cec144ec06cdfbc1f5e0e0a3e
Gustavo Bonifácio <gustavobonifaciodeoliveira27@gmail.com>
2025-11-27 22:34:38 -0300

    feat(denuncias): Implementa teste de sucesso para criarDenuncia.
    Co-authored-by: Lucas Oliveira <lucas.odpaula@gmail.com>

--- Back/src/denuncias/denuncias.service.spec.ts
+++ Back/src/denuncias/denuncias.service.spec.ts
@@ -54,4 +54,29 @@ describe('DenunciasService', () => {
     service = module.get<DenunciasService>(DenunciasService);
     jest.clearAllMocks();
   });
+  describe('criarDenuncia', () => {
+    it('[Sucesso] Deve criar a denúncia vinculando o idUsuario e verificar a categoria', async () => {
+      mockPrismaService.categoria.findUnique.mockResolvedValue({ id: 2 });
+      
+      const idUsuarioAutor = 10;
+      await service.criarDenuncia(idUsuarioAutor, mockDto as any);
+
+      expect(mockPrismaService.denuncia.create).toHaveBeenCalledWith(
+        expect.objectContaining({
+          data: expect.objectContaining({
+            idUsuario: idUsuarioAutor,
+            idCategoria: mockDto.idCategoria
+          })
+        })
+      );
+    });
+
+    it('[Erro] Deve lançar NotFoundException se a Categoria não for encontrada', async () => {
+      mockPrismaService.categoria.findUnique.mockResolvedValue(null);
+      
+      await expect(service.criarDenuncia(10, mockDto as any)).rejects.toThrow(
+        new NotFoundException('Categoria não encontrada!'),
+      );
+    });
+  });
 });
