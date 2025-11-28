52257e4cbdc11e7bdfe16ec245d6d947131a108d
Pedro Lucas <21pedrolucas21@gmail.com>
2025-11-27 22:35:28 -0300

    feat: adiciona botões 2 a 4

--- Front/src/app/(orientacao)/orientacao/page.tsx
+++ Front/src/app/(orientacao)/orientacao/page.tsx
@@ -32,22 +32,48 @@ export default function Orientacao() {
             </h3>
           </div>
         </Link>
+
+        <Link href="/assedio" className="
+          bg-linear-to-br from-blue-400 to-blue-500
+          shadow-md rounded-2xl overflow-hidden
+          flex flex-col justify-between min-h-[120px]
+          transition-transform duration-300 hover:scale-105"
+        >
+          <div className="p-8 text-white flex flex-col items-center gap-4">
+            <ArrowsUpDownIcon className="w-12 h-12" />
+            <h3 className="text-lg sm:text-xl font-semibold text-center leading-tight">
+              Assedio
+            </h3>
+          </div>
+        </Link>
+
+        <Link href="/direitos-humanos" className="
+          bg-linear-to-br from-blue-400 to-blue-500
+          shadow-md rounded-2xl overflow-hidden
+          flex flex-col justify-between min-h-[120px]
+          transition-transform duration-300 hover:scale-105"
+        >
+          <div className="p-8 text-white flex flex-col items-center gap-4">
+            <ArrowsUpDownIcon className="w-12 h-12" />
+            <h3 className="text-lg sm:text-xl font-semibold text-center leading-tight">
+              Direitos Humanos
+            </h3>
+          </div>
+        </Link>
               
-              <Link href="/orientacao/assedio" className="p-6 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 text-center">
-                <h2 className="text-xl font-semibold">Assedio</h2>
-              </Link>
-              <Link href="/orientacao/direitos-humanos" className="p-6 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 text-center">
-                <h2 className="text-xl font-semibold">Direitos Humanos</h2>
-              </Link>
-              <Link href="/orientacao/infraestrutura" className="p-6 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 text-center">
-                <h2 className="text-xl font-semibold">Infraestrutura</h2>
-              </Link>
-              <Link href="/orientacao/integridade" className="p-6 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 text-center">
-                <h2 className="text-xl font-semibold">Integridade</h2>
-              </Link>
-              <Link href="/orientacao/melhorias" className="p-6 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 text-center">
-                <h2 className="text-xl font-semibold">Melhorias</h2>
-              </Link>
+        <Link href="/infraestrutura" className="
+          bg-linear-to-br from-blue-400 to-blue-500
+          shadow-md rounded-2xl overflow-hidden
+          flex flex-col justify-between min-h-[120px]
+          transition-transform duration-300 hover:scale-105"
+        >
+          <div className="p-8 text-white flex flex-col items-center gap-4">
+            <ArrowsUpDownIcon className="w-12 h-12" />
+            <h3 className="text-lg sm:text-xl font-semibold text-center leading-tight">
+              Infraestrutura
+            </h3>
+          </div>
+        </Link>
 
             </div>
           </div>
