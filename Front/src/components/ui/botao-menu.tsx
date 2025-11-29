aad1ac752a2bfe339b1291bc796c785e0e1454e2
juliagabriellafs <juliagabriellaf0407@gmail.com>
2025-11-28 21:49:24 -0300

    feat - cria estrutura base e interface do componente BotaoMenu
    Co-authored-by: Pwdrinho <21pedrolucas21@email.com>

--- Front/src/components/ui/botao-menu.tsx
+++ Front/src/components/ui/botao-menu.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 interface BotaoMenuProps {
-    
+  texto: string;
+  icone: React.ReactNode;
+  onClick?: () => void;
 }
 
-export default function BotaoMenu() {
-    return;
+export default function BotaoMenu({ texto, icone, onClick }: BotaoMenuProps) {
+  return (
+    <button onClick={onClick}>
+      <div>
+        {icone}
+      </div>
+      <span>
+        {texto}
+      </span>
+    </button>
+  );
 }
