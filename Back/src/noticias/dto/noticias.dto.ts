de5699bf311a7e282907a107f7a20e942bed6c10
Lucas Alves <lucastouram@gmail.com>
2025-12-04 17:39:04 -0300

    feat: Adciona validacao de descricao no DTO de noticias Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/noticias/dto/noticias.dto.ts
+++ Back/src/noticias/dto/noticias.dto.ts
@@ -1,7 +1,11 @@
-import { IsString, IsNotEmpty, IsInt, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, IsInt, IsOptional, isString } from 'class-validator';
 
 export class NoticiasDto {
     @IsInt({message: 'O id do usuario deve ser um número'})
-    @IsNotEmpty({message: 'O id do usuario nao pode estar vazio '})
+    @IsNotEmpty({message: 'O id do usuario nao pode estar vazio'})
     idUsuario:number;
+
+    @IsString({message: 'A descricao deve ser um texto'})
+    @IsNotEmpty({message: 'A descricao nao pode estar vazia'})
+    descricao:string;
 }
