7a062c0c9a7518fe17050a321c8cd0e612d7e594
Lucas Alves <lucastouram@gmail.com>
2025-12-04 18:31:09 -0300

    feat: Adciona validacao de id do usuario no DTO de edicao de noticias Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/noticias/dto/edicao.noticias.dto.ts
+++ Back/src/noticias/dto/edicao.noticias.dto.ts
@@ -1,1 +1,10 @@
-import { IsInt, IsString, IsOptional } from "class-validator";
+import { IsInt, IsString, IsOptional, isInt } from "class-validator";
+
+export class edicaoNoticiasDto {
+
+    @IsInt({message: 'O id do usuario deve ser um número.'})
+    @IsOptional()
+    idUsuario:number;
+
+    
+}
