b02b60ff6c3e75afc0818ee60f8778618f659424
Lucas Alves <lucastouram@gmail.com>
2025-12-04 17:32:14 -0300

    feat: Valida o id do usuario no DTO de Noticias Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/noticias/dto/noticias.dto.ts
+++ Back/src/noticias/dto/noticias.dto.ts
@@ -1,1 +1,7 @@
 import { IsString, IsNotEmpty, IsInt, IsOptional } from 'class-validator';
+
+export class NoticiasDto {
+    @IsInt({message: 'O id do usuario deve ser um número'})
+    @IsNotEmpty({message: 'O id do usuario nao pode estar vazio '})
+    idUsuario:number;
+}
