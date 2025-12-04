4e8b92506de8047fdc12872aaa2a7f7cc0512676
Gabriel <12gabrieldiniz@gmail.com>
2025-12-04 14:35:33 -0300

    feat: adição dos imports e dependências necessárias

--- Front/src/components/modalEditarNoticia.tsx
+++ Front/src/components/modalEditarNoticia.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import api from '@/utils/api'; 
+import { toast } from 'sonner';
+
 export default function ModalEditarNoticia() {
   return null;
 }
