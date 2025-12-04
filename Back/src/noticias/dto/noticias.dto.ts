bba7b68792a2ad4194cfe391452f01334c50f78e
Lucas Alves <lucastouram@gmail.com>
2025-12-04 18:14:37 -0300

    feat: Aciona validacao de tipo no DTO de noticias Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/noticias/dto/noticias.dto.ts
+++ Back/src/noticias/dto/noticias.dto.ts
@@ -1,13 +1,17 @@
 import { IsString, IsNotEmpty, IsInt, IsOptional, isString } from 'class-validator';
 
 export class NoticiasDto {
-    @IsInt({message: 'O id do usuario deve ser um número'})
-    @IsNotEmpty({message: 'O id do usuario nao pode estar vazio'})
+    @IsInt({message: 'O id do usuario deve ser um número.'})
+    @IsNotEmpty({message: 'O id do usuario nao pode estar vazio.'})
     idUsuario:number;
 
-    @IsString({message: 'A descricao deve ser um texto'})
-    @IsNotEmpty({message: 'A descricao nao pode estar vazia'})
+    @IsString({message: 'A descricao deve ser um texto.'})
+    @IsNotEmpty({message: 'A descricao nao pode estar vazia.'})
     descricao:string;
+
+    @IsString({message: 'O tipo deve ser um texto.'})
+    @IsNotEmpty({message: 'O tipo nao pode estar vazio.'})
+    tipo:string
 
     @IsString({ message: 'A mediaSrc deve ser um texto.' })
     @IsOptional()    
