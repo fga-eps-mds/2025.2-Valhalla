02624c4ed54874eb2c26bb727416fa13ef888bf6
Lucas Alves <lucastouram@gmail.com>
2025-12-04 23:12:16 -0300

    feat: importa o service para o controller de noticias
    Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/noticias/noticias.controller.ts
+++ Back/src/noticias/noticias.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller , Post, Param, ParseIntPipe, Patch, Delete, Get, Request, Query, UseGuards } from '@nestjs/common';
+import { NoticiasService } from './noticias.service';
 
 @Controller('noticias')
 export class NoticiasController {
