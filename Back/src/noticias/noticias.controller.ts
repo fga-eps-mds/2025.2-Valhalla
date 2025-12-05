ea9896aa10c365aaea68c0ef4261ae8a80eaae4d
Lucas Alves <lucastouram@gmail.com>
2025-12-04 23:16:53 -0300

    feat: Importa o decorador de authenticação isPublic para o controller de noticias
    Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/noticias/noticias.controller.ts
+++ Back/src/noticias/noticias.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller , Post, Param, ParseIntPipe, Patch, Delete, Get, Request, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller , Post, Param, ParseIntPipe, Patch, Delete, Get, Request, Query, UseGuards, DefaultValuePipe } from '@nestjs/common';
 import { NoticiasService } from './noticias.service';
 import { NoticiasDto } from './dto/noticias.dto';
 import { EdicaoNoticiasDto } from './dto/edicao.noticias.dto';
-
+import { IsPublic } from 'src/auth/decorators/isPublic.decorator';
 
 @Controller('noticias')
 export class NoticiasController {
