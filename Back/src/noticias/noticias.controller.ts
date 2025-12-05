399039216c88069e962209dc06886e6f6524fe28
Lucas Alves <lucastouram@gmail.com>
2025-12-04 23:14:44 -0300

    feat: Importa os DTOs de noticias para o controller de noticias
    Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/noticias/noticias.controller.ts
+++ Back/src/noticias/noticias.controller.ts
@@ -1,5 +1,8 @@
 import { Body, Controller , Post, Param, ParseIntPipe, Patch, Delete, Get, Request, Query, UseGuards } from '@nestjs/common';
 import { NoticiasService } from './noticias.service';
+import { NoticiasDto } from './dto/noticias.dto';
+import { EdicaoNoticiasDto } from './dto/edicao.noticias.dto';
+
 
 @Controller('noticias')
 export class NoticiasController {
