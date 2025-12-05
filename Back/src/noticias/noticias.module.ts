65659a41b5e1078599f994d2de4f69224f9b4f93
AntonioFtb <toin.lucas15@gmail.com>
2025-12-05 11:32:14 -0300

    refactor(noticias): Adiciona imports de Controller e Service ao módulo
    Co-authored-by: xLucasMelo <lucastouram@gmail.com>

--- Back/src/noticias/noticias.module.ts
+++ Back/src/noticias/noticias.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { NoticiasService } from './noticias.service';
+import { NoticiasController } from './noticias.controller';
 
 @Module({})
 export class NoticiasModule {}
