08ae6a7cc504a9cee43762c5587032720b52f549
Lucas Alves <lucastouram@gmail.com>
2025-12-04 23:49:35 -0300

    feat: Cria o metodo de editarNoticia no controller de noticias
    Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/noticias/noticias.controller.ts
+++ Back/src/noticias/noticias.controller.ts
@@ -3,6 +3,7 @@ import { NoticiasService } from './noticias.service';
 import { NoticiasDto } from './dto/noticias.dto';
 import { EdicaoNoticiasDto } from './dto/edicao.noticias.dto';
 import { IsPublic } from 'src/auth/decorators/isPublic.decorator';
+import { AuthRequest } from 'src/auth/models/authRequest';
 
 @Controller('noticias')
 export class NoticiasController {
@@ -10,9 +11,19 @@ export class NoticiasController {
 
     @Post()
     async criarNoticia(
-        @Request() req,
+        @Request() req: AuthRequest,
         @Body() data: NoticiasDto,
     ) {
         return this.noticiasService.criarNoticia(req.user.id, data);
     }
+
+    @Patch(':id')
+    async editarNoticia(
+        @Param('id', ParseIntPipe) id: number,
+        @Request() req: AuthRequest,
+        @Body() data: EdicaoNoticiasDto,
+    ) {
+        return this.noticiasService.editarNoticia(id, req.user.id, data);
+    }
+
 }
