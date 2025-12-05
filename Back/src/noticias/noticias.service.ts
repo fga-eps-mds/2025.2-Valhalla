325d60ee5befadcd0888186ae1824c33c0a207f2
Lucas Alves <lucastouram@gmail.com>
2025-12-05 00:17:33 -0300

    feat: Cria somente o scopo das funçoes no service de noticias
    Co-authored-by: Devv-Antonio <antonio.lucasa06@gmail.com>

--- Back/src/noticias/noticias.service.ts
+++ Back/src/noticias/noticias.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { NoticiasDto } from './dto/noticias.dto';
+import { EdicaoNoticiasDto } from './dto/edicao.noticias.dto';
+
 
 @Injectable()
-export class NoticiasService {}
+export class NoticiasService {
+      async criarNoticia(idUsuario: number, data: NoticiasDto) {
+  }
+
+    async editarNoticia(id: number, idUsuario: number, data: EdicaoNoticiasDto) {}
+
+    async deletarDenuncia(id: number, idUsuario: number, tipoUsuario: string) {}
+
+    async desativarDenuncia(id: number, idUsuario: number, tipoUsuario: string) {}
+
+    async encontrarDenuncia(id: number) {}
+
+    async listarDenuncias(page: number, limit: number) {}
+
+    async listarDenunciasPorUsuario(idUsuario: number, page: number, limit: number) {}
+
+}
