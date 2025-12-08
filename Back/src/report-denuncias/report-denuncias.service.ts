381c6e85ee281dbfcfd7479b4a373d57f10f9b8d
pedroiaan <pedroianx8@gmail.com>
2025-12-08 12:16:22 -0300

    feat - criação da lógica de criar report no service
    Co-authored-by: Gustavo Bonifácio <gustavobonifaciodeoliveira27@gmail.com>

--- Back/src/report-denuncias/report-denuncias.service.ts
+++ Back/src/report-denuncias/report-denuncias.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { reportDenunciasDto } from './dto/report-denuncias.dto';
 
 @Injectable()
-export class ReportDenunciasService {}
+export class ReportDenunciasService {
+    constructor (private prisma: PrismaService) {}
+
+    async CriarReportDenuncia (data: reportDenunciasDto) {
+        const reportDenuncia = await this.prisma.reportsDenuncia.create({data});
+    }
+}
