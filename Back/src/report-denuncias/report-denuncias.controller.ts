b60550d5430fbc714240542f1f82fdfc715cfd6a
pedroiaan <pedroianx8@gmail.com>
2025-12-08 12:29:45 -0300

    feat - criação do get (acharTodos) no controller ReportDenuncias
    Co-authored-by: Gustavo Bonifácio <gustavobonifaciodeoliveira27@gmail.com>

--- Back/src/report-denuncias/report-denuncias.controller.ts
+++ Back/src/report-denuncias/report-denuncias.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ReportDenunciasService } from './report-denuncias.service';
 import { reportDenunciasDto } from './dto/report-denuncias.dto';
 
@@ -9,5 +9,9 @@ export class ReportDenunciasController {
     @Post()
     async CriarReport(@Body() data: reportDenunciasDto){
         return this.reportDenunciasService.CriarReportDenuncia(data);
+    }
+    @Get()
+    async acharTodos(){
+        return this.reportDenunciasService.acharTodosReports();
     }
 }
