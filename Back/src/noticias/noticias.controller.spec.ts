b810a53de554a13bc6a41be7b721fcbb36893aff
Devv-Antonio <antonio.lucasa06@gmail.com>
2025-12-08 00:06:32 -0300

    test(noticias): corrige injeção de dependência nos testes do controller

--- Back/src/noticias/noticias.controller.spec.ts
+++ Back/src/noticias/noticias.controller.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NoticiasController } from './noticias.controller';
+import { NoticiasService } from './noticias.service';
 
 describe('NoticiasController', () => {
   let controller: NoticiasController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [NoticiasController],
-    }).compile();
 
-    controller = module.get<NoticiasController>(NoticiasController);
-  });
+const mockNoticiasService = {
+  create: jest.fn(),
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+  update: jest.fn(),
+  remove: jest.fn(),
+  desativarNoticia: jest.fn(), 
+};
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+beforeEach(async () => {
+  const module: TestingModule = await Test.createTestingModule({
+    controllers: [NoticiasController],
+    providers: [
+      {
+        provide: NoticiasService,
+        useValue: mockNoticiasService,
+      },
+    ],
+  }).compile();
+
+  controller = module.get<NoticiasController>(NoticiasController);
+});
 });
