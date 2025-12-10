7b9fc37f99011eedff97c6011a146347392d53e6
Gustavo Bonifácio <gustavobonifaciodeoliveira27@gmail.com>
2025-12-10 13:22:15 -0300

    chore: configura modulo de apoio denuncia
    Co-authored-by: pedroiaan <pedroianx8@gmail.com>

--- Back/src/apoio-denuncia/apoio-denuncia.module.ts
+++ Back/src/apoio-denuncia/apoio-denuncia.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApoioDenunciaService } from './apoio-denuncia.service';
 import { ApoioDenunciaController } from './apoio-denuncia.controller';
+import { PrismaService } from '../database/prisma.service'; 
 
 @Module({
   controllers: [ApoioDenunciaController],
-  providers: [ApoioDenunciaService],
+  providers: [ApoioDenunciaService, PrismaService],
 })
 export class ApoioDenunciaModule {}
