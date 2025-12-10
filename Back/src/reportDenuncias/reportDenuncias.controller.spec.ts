37264db06d0d4836048abbacdfec937eb2155bc5
Pedro Americo <dev.americo@gmail.com>
2025-12-09 22:07:32 -0300

    refactore: renomeacao de teste controller report de denuncia ao padrao camelCode
    Co-authored-by: juliagabriellafs <juliagabriellafs@users.noreply.github.com>

--- Back/src/reportDenuncias/reportDenuncias.controller.spec.ts
+++ Back/src/reportDenuncias/reportDenuncias.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportDenunciasController } from './reportDenuncias.controller';
 import { ReportDenunciasService } from './reportDenuncias.service';
-import { reportDenunciasDto } from './dto/report-denuncias.dto';
+import { reportDenunciasDto } from './dto/reportDenuncias.dto';
 
 describe('ReportDenunciasController', () => {
   let controller: ReportDenunciasController;
