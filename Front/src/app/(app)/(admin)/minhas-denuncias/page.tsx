1a83c6608412ab60c4fce3bc58ebe7aabf3a4ecb
Pedro Americo <dev.americo@gmail.com>
2025-12-11 12:13:47 -0300

    feat: criacao de TipoNoticia em types
    Co-authored-by: juliagabriellafs <juliagabriellafs@users.noreply.github.com>

--- Front/src/app/(app)/(admin)/minhas-denuncias/page.tsx
+++ Front/src/app/(app)/(admin)/minhas-denuncias/page.tsx
@@ -6,8 +6,6 @@ import { useEffect, useState } from "react";
 import { toast } from 'sonner';
 import api from "@/utils/api";
 import CardDenuncia from "@/components/ui/card-denuncia-gerencia";
-import BotaoMenu from "@/components/ui/botao-menu";
-import { UsersIcon, ChatBubbleLeftIcon } from "@heroicons/react/24/solid";
 import ModalExcluirDenunciaSoft from "@/components/modalExcluirDenunciaSoft";
 import ModalEditarDenuncia from "@/components/modalEditarDenuncia";
 
